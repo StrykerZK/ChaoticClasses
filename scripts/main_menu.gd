b8eb807c7fc2bb4afa7bd3c3c671300396b10af9
Zwe <zwe.mk24@gmail.com>
2025-02-14 05:25:46 -0800

    added Main Menu UI

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -2,11 +2,21 @@ extends Control
 
 @export var main_game: PackedScene
 
-func _ready() -> void:
-	pass
+func _enter_tree() -> void:
+	$TitleMenu.show()
+	$PlayMenu.hide()
 
 func _process(delta: float) -> void:
 	pass
 
-func _on_button_pressed() -> void:
-	get_tree().change_scene_to_packed(main_game)
+func start_game() -> void:
+	$TitleMenu.hide()
+	$PlayMenu.show()
+
+
+func _on_settings_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
